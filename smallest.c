17d2e6466f35cc6dfa7dd8ec26ce551d3ffdcfd1
Aman Kamal <109606739+KAMALAMAN687@users.noreply.github.com>
2022-11-18 01:40:22 +0530

    Create smallest.c

---
+++ smallest.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    printf("enter the size of array");
+    scanf("%d",&a);
+    printf("enter the elements of array");
+
+    int b[a];
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&b[i]);
+
+    }
+    smallest(b,a);
+
+}
+int smallest(int c[],int n)
+{
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+           if(c[i]<c[j])
+           {
+
+           }
+           else
+           break;                            
+        }
+        if(j==n)
+        {
+            printf("min  value is %d",c[i]);
+            return c[i];
+        }
+
+    }
+
+}
